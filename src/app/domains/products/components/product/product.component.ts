73fd41ae4477ab3585b026af9b86245a1f705e54
Israel Garcia <isragarciahdez@gmail.com>
2023-11-24 12:23:45 -0600

    Creando componente de productos

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../../shared/models/Product.model';
 
 @Component({
   selector: 'app-product',
@@ -9,14 +10,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  @Input({required: true}) img: string = '';
-  @Input({required: true}) price: number = 0;
-  @Input({required: true}) title: string = '';
+  @Input({required: true}) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler(){
     console.log('Click desde el hijo');
-    this.addToCart.emit('Hola desde emit()' + this.title);
+    this.addToCart.emit('Hola desde emit()');
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from "../../../products/components/product/product.component";
+import { Product } from '../../../shared/models/Product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,50 @@ import { ProductComponent } from "../../../products/components/product/product.c
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor(){
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 100,
+        image: 'https://picsum.photos/200/200?r=23',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 200,
+        image: 'https://picsum.photos/200/200?r=24',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 3',
+        price: 300,
+        image: 'https://picsum.photos/200/200?r=25',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 4',
+        price: 400,
+        image: 'https://picsum.photos/200/200?r=26',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 5',
+        price: 500,
+        image: 'https://picsum.photos/200/200?r=27',
+        createdAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string){
     console.log('Estamos en el padre');
     console.log(event);        
