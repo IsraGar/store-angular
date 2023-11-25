e8b06fea061fe51934abe1e7fd89aea938de2173
Israel Garcia <isragarciahdez@gmail.com>
2023-11-25 17:47:13 -0600

    Creando el carrito de compras

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  hideSideMenu = signal(true);
 
+  toogleSideMenu(){
+    this.hideSideMenu.update(prevState => !prevState);
+  }
 }
